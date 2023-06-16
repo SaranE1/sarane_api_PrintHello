6351a53d09ab4339eb560a90c11cc868e960c520
intrepid Saran <saranintrepid@gmail.com>
2023-06-16 17:26:45 +0530

    first

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create the Flask app
+app = Flask(__name__)
+
+# Define the API endpoint
+@app.route('/api/printHello', methods=['GET'])
+def print_hello():
+    return 'Hello'
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run()
